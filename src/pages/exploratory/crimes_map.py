c5a5aa8f11373c25103695a3570c4e21bc16bdb0
Anthony Vargas <tony_jason@hotmail.com>
2022-06-10 23:29:29 -0500

    Add example map

--- src/pages/exploratory/crimes_map.py
+++ src/pages/exploratory/crimes_map.py
@@ -1,3 +1,15 @@
-from dash import  html
+from dash import dcc, html
+import plotly.express as px
 
-map_layout = html.Div("Here goes the crimes map...", id="crimes_map")
+df = px.data.election()
+geojson = px.data.election_geojson()
+
+fig = px.choropleth_mapbox(df, geojson=geojson, color="Bergeron",
+                           locations="district", featureidkey="properties.district",
+                           center={"lat": 45.5517, "lon": -73.7073},
+                           mapbox_style="carto-positron", zoom=9)
+
+map_layout = html.Div([
+    dcc.Graph(figure=fig)
+    ]
+    , id="crimes_map")
